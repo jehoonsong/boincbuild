54267abd2c70abebc5f975b895814ca3643ee1e2
Jehoon Song <jehoon.song@net-targets.com>
2025-05-03 20:41:44 +0900

    .

--- jhs/images/boincc/bin/start-boinc.sh
+++ jhs/images/boincc/bin/start-boinc.sh
@@ -4,5 +4,9 @@
 echo $BOINC_GUI_RPC_PASSWORD > /var/lib/boinc/gui_rpc_auth.cfg
 echo $BOINC_REMOTE_HOST > /var/lib/boinc/remote_hosts.cfg
 
+crontab -l 2>/dev/null | grep -q "boinccmd" || \
+    (crontab -l 2>/dev/null; echo "*/5 * * * * boinccmd --project $BOINC_PROJECT_URL update") | crontab -
+
 # Run BOINC. Full path needs for GPU support.
 exec /usr/bin/boinc $BOINC_CMD_LINE_OPTIONS
+
