add81b32477db8c5114717693b1a92f07f03a385
Jehoon Song <jehoon.song@net-targets.com>
2025-05-05 18:35:47 +0900

    .

--- ndist/data/setup_boinc.sh
+++ ndist/data/setup_boinc.sh
@@ -5,10 +5,14 @@ cp -v /data/app_config.xml /home/boincadm/project/app_config.xml
 crontab -l 2>/dev/null | grep -q "bin/start --cron" || \
 (crontab -l 2>/dev/null; echo "*/5 * * * * cd /home/boincadm/project && bin/start --cron") | crontab -
 
+cp project.xml /home/boincadm/project 
+
 chown boincadm:boincadm -Rv /home/boincadm/project/upload
 chown boincadm:boincadm -Rv /home/boincadm/project/download
 chown boincadm:boincadm -Rv /home/boincadm/project/*
-cp project.xml /home/boincadm/project 
+chmod 644 /home/boincadm/project/project.xml
+chmod 644 /home/boincadm/project/config.xml
+
 cd /home/boincadm/project && bin/xadd 
 cd /home/boincadm/project && bin/update_versions
 
