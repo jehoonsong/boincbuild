76eb91173034b05b5c89fe3848f4904b6570fc2f
Jehoon Song <song.jehoon@gmail.com>
2025-05-06 17:06:22 +0900

    .

--- ndist/data/myapp/25/x86_64-pc-linux-gnu/myapp_25_x86_64-pc-linux-gnu.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if grep -q '<soft_link>' INPUT.tgz; then
-    TEXT=$(grep -oP '(?<=<soft_link>).*?(?=</soft_link>)' INPUT.tgz) && cp $TEXT ./
-fi
-
-tar cvzf OUTPUT.tgz .
-
-echo "bye!"
